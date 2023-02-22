cd11aaca789cbf603ab865265a3045f68159cae0
mrcabbage972 <mayvic@gmail.com>
2023-02-21 20:32:49 -0500

    Fixing calculator tool name casing

--- src/toolformer/tools/calculator.py
+++ src/toolformer/tools/calculator.py
@@ -3,7 +3,7 @@ from toolformer.tool import Tool
 
 class CalculatorTool(Tool):
     def get_tool_name(self):
-        return 'calculator'
+        return 'CALCULATOR'
 
     def get_prompt_template(self) -> str:
         return """
@@ -13,22 +13,22 @@ You can call the API by writing "[Calculator(expression)]" where "expression" is
 Here are some examples of API calls:
 
 Input: The number in the next term is 18 + 12 x 3 = 54.
-Output: The number in the next term is 18 + 12 x 3 = [Calculator(18 + 12 * 3)] 54.
+Output: The number in the next term is 18 + 12 x 3 = [CALCULATOR(18 + 12 * 3)] 54.
 
 Input: The population is 658,893 people. This is 11.4% of the national average of 5,763,868 people. 
-Output: The population is 658,893 people. This is 11.4% of the national average of [Calculator(658,893 / 11.4%)] 5,763,868 people. 
+Output: The population is 658,893 people. This is 11.4% of the national average of [CALCULATOR(658,893 / 11.4%)] 5,763,868 people. 
 
 Input: A total of 252 qualifying matches were played, and 723 goals were scored (an average of 2.87 per match). This is three times less than the 2169 goals last year. 
-Output: A total of 252 qualifying matches were played, and 723 goals were scored (an average of [Calculator(723 / 252)] 2.87 per match). This is twenty goals more than the [Calculator(723 - 20)] 703 goals last year. 
+Output: A total of 252 qualifying matches were played, and 723 goals were scored (an average of [CALCULATOR(723 / 252)] 2.87 per match). This is twenty goals more than the [CALCULATOR(723 - 20)] 703 goals last year. 
 
 Input: I went to Paris in 1994 and stayed there until 2011, so in total, it was 17 years. 
-Output: I went to Paris in 1994 and stayed there until 2011, so in total, it was [Calculator(2011 - 1994)] 17 years. 
+Output: I went to Paris in 1994 and stayed there until 2011, so in total, it was [CALCULATOR(2011 - 1994)] 17 years. 
 
 Input: From this, we have 4 * 30 minutes = 120 minutes. 
-Output: From this, we have 4 * 30 minutes = [Calculator(4 * 30)] 120 minutes. 
+Output: From this, we have 4 * 30 minutes = [CALCULATOR(4 * 30)] 120 minutes. 
 
-Input: x 
-Output: {}"""
+Input: {} 
+Output: """
 
     def run(self, input: str) -> str:
         # TODO: the following code should be a method in the Tool class.
@@ -50,4 +50,4 @@ Output: {}"""
 
 
 if __name__ == '__main__':
-    print(CalculatorTool().run('asdadsad [CALCULATOR(723 / 252000)] asd'))
+    print(CalculatorTool().run('asdadsad [Calculator(723 / 252000)] asd'))
